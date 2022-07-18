2785540a6a7a74792800696a15a5c5e2a5bc45b7
Undead34 <maizogabriel@gmail.com>
2022-07-17 21:30:20 -0400

    Cismu Player

--- src/app/events/interaction/localSounds/reload-local-sounds-db.js
+++ src/app/events/interaction/localSounds/reload-local-sounds-db.js
@@ -1,37 +1,5 @@
-// // const Database = require("../../../modules/database/database");
-// const Database = require("../../../modules/database/better-database");
-// const { readdirSync } = require("fs");
-
-// module.exports = {
-//   name: "localSounds:reload-local-sounds-db",
-//   isHandle: true,
-//   isIPCMain: true,
-//   once: false,
-//   autostart: false,
-//   action: async (e) => {
-//     try {
-//       let musicsFiles = await readdirSync(global.appPaths.useMusic).filter(file => {
-//         return file.endsWith("mp3") || file.endsWith("flac");
-//       })
-      
-//       let db = new Database(global.appPaths.dbPath);
-
-//       console.time("t1")
-//       await db.insertMusic(musicsFiles);
-//       console.timeEnd("t1")
-//       console.log(`${musicsFiles.length} songs scanned in:`)
-      
-//       let musics = await db.getAllMusics();
-
-//       db.closeDatabase();
-
-//       return musics;
-//     } catch (error) {
-//       console.log(error)
-//       return [];
-//     }
-//   }
-// }
+const Database = require("../../../common/database/better-database");
+const { readdirSync } = require("fs");
 
 module.exports = {
   name: "localSounds:reload-local-sounds-db",
@@ -40,6 +8,28 @@ module.exports = {
   once: false,
   autostart: false,
   action: async (e) => {
-    
+    try {
+      let musicsFiles = await readdirSync(global.paths.music).filter(file => {
+        return file.endsWith("mp3") || file.endsWith("flac");
+      })
+
+      let database = global.database.db;
+      if (!database.db.open) {
+        global.database.db = new Database(this.paths.database);
+        database = global.database.db;
+      }
+
+      console.time("t1")
+      await database.insertMusic(musicsFiles);
+      console.timeEnd("t1")
+      console.log(`${musicsFiles.length} songs scanned in:`)
+
+      let musics = await database.getAllMusics();
+
+      return musics;
+    } catch (error) {
+      console.log(error)
+      return [];
+    }
   }
 }
